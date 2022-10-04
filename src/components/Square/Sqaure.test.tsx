9adcc31536d6e0547cc7ea04a9a99edac4e3c371
Amiel Nava <amieln@icloud.com>
2022-10-03 17:04:33 -0700

    Cleanup the testing file for Square to be more modular.

--- src/components/Square/Sqaure.test.tsx
+++ src/components/Square/Sqaure.test.tsx
@@ -1,64 +1,63 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SquareType } from "../../types/GameTypes";
 import Square from "./Square";
 import styles from "./Square.module.css";
 
-test("Square has one button", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
+describe("Square Component", () => {
+  test("has one button", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    render(<Square mark="" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
+    const buttonElement = screen.getByRole("button");
 
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toBeInTheDocument();
-});
+    expect(buttonElement).toBeInTheDocument();
+  });
 
-test("Button element renders X when passed by props", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="X" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toHaveTextContent("X");
-});
+  test("button element renders the square type passed by props", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    const squareTypes: SquareType[] = ["X", "O", ""];
+    for (const sqType of squareTypes) {
+      render(<Square mark={sqType} highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
+      const buttonElement = screen.getByRole("button");
 
-test("Button element renders O when passed by props", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="O" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toHaveTextContent("O");
-});
+      expect(buttonElement).toHaveTextContent(sqType);
+      cleanup(); // Cleanup is only done automatically after each test("", () => {})
+    }
+  });
 
-test("Button element renders an empty string when passed by props", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toHaveTextContent("");
-});
+  test("button highlights to color yellow when highlight props is true", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    render(<Square mark="" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
+    const buttonElement = screen.getByRole("button");
 
-test("Button highlights to color yellow when highlight props is true", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const buttonElement = screen.getByRole("button");
-  expect(buttonElement).toHaveClass(styles.highlight);
-});
+    expect(buttonElement).toHaveClass(styles.highlight);
+  });
 
-test("Button calls onClick function when clicked and returns its index value", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  const index = 7;
+  test("button calls onClick function when clicked and returns its index value", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    const index = 7;
 
-  render(<Square mark="" highlight={true} squareIndex={index} onSelectSquare={mockCallBack} />);
-  const buttonElement = screen.getByRole("button");
-  fireEvent.click(buttonElement);
-  expect(mockCallBack.mock.calls.length).toBe(1);
-  expect(mockCallBack.mock.results[0].value).toBe(index);
-});
+    render(<Square mark="" highlight={true} squareIndex={index} onSelectSquare={mockCallBack} />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
 
-test("Square has Player X color when mark is X", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="X" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const xPlayer = screen.getByText(/X/);
-  expect(xPlayer).toHaveClass(styles.playerX);
-});
+    expect(mockCallBack.mock.calls.length).toBe(1);
+    expect(mockCallBack.mock.results[0].value).toBe(index);
+  });
 
-test("Square has Player O color when mark is O", () => {
-  const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-  render(<Square mark="O" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
-  const xPlayer = screen.getByText(/O/);
-  expect(xPlayer).toHaveClass(styles.playerO);
+  test("square has player's X css class when mark is X", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    render(<Square mark="X" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
+    const xPlayer = screen.getByText(/X/);
+
+    expect(xPlayer).toHaveClass(styles.playerX);
+  });
+
+  test("square has player's O css class color when mark is O", () => {
+    const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
+    render(<Square mark="O" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
+    const xPlayer = screen.getByText(/O/);
+
+    expect(xPlayer).toHaveClass(styles.playerO);
+  });
 });
