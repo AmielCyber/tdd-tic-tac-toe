954b855356ffb6ae0606c57c59749ca1fa6696db
Amiel Nava <amieln@icloud.com>
2022-10-05 16:39:03 -0700

    Added button classes.
    Removed the class in the p element and added it to the button class. Now button classes can be made of up to three classes (empty, player, and square).

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,18 +1,17 @@
 import type { SquareType } from "../../types/GameTypes";
 import cssClass from "./Square.module.css";
 
-// Button Styles
-const REGULAR = cssClass.square;
-const HIGHLIGHTED = cssClass.square + " " + cssClass.highlight;
-
-function getPlayerColor(mark: SquareType) {
+function getPlayerColor(mark: SquareType): string {
+  return mark === "X" ? cssClass.playerX : cssClass.playerO;
+}
+function getButtonClass(mark: SquareType, highlight: boolean): string {
   if (mark === "") {
-    return "";
-  } else if (mark === "X") {
-    return cssClass.playerX;
-  } else {
-    return cssClass.playerO;
+    return `${cssClass.square} ${cssClass.emptySquare}`;
   }
+  if (highlight) {
+    return `${cssClass.square} ${cssClass.highlight} ${getPlayerColor(mark)}`;
+  }
+  return `${cssClass.square} ${getPlayerColor(mark)}`;
 }
 
 type SquareProps = {
@@ -23,12 +22,11 @@ type SquareProps = {
 };
 
 const Square = ({ squareIndex, mark, highlight, onSelectSquare }: SquareProps) => {
-  const buttonStyle = highlight ? HIGHLIGHTED : REGULAR;
-  const playerStyle = getPlayerColor(mark);
+  const buttonStyle = getButtonClass(mark, highlight);
   const squareName = `Row ${Math.floor(squareIndex / 3) + 1}, Column ${(squareIndex % 3) + 1}`;
   return (
     <button name={squareName} className={buttonStyle} onClick={() => onSelectSquare(squareIndex)}>
-      <p className={playerStyle}>{mark}</p>
+      <p>{mark}</p>
     </button>
   );
 };
