a3121000e49267d2b40b3f82febfff305c284cd0
Amiel Nava <amieln@icloud.com>
2022-10-05 16:34:00 -0700

    Will modify square button classes
    Will modify square button to have at  most three types of classes.

--- src/components/Square/Sqaure.test.tsx
+++ src/components/Square/Sqaure.test.tsx
@@ -25,9 +25,9 @@ describe("Square Component", () => {
     }
   });
 
-  test("button highlights to color yellow when highlight props is true", () => {
+  test("button is highlighted when highlight props is true", () => {
     const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-    render(<Square mark="" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
+    render(<Square mark="X" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
     const buttonElement = screen.getByRole("button");
 
     expect(buttonElement).toHaveClass(styles.highlight);
@@ -47,17 +47,19 @@ describe("Square Component", () => {
 
   test("square has player's X css class when mark is X", () => {
     const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
-    render(<Square mark="X" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
-    const xPlayer = screen.getByText(/X/);
+    render(<Square mark="X" highlight={false} squareIndex={0} onSelectSquare={mockCallBack} />);
+    const xPlayerButton = screen.getByRole("button");
 
-    expect(xPlayer).toHaveClass(styles.playerX);
+    expect(xPlayerButton).toHaveTextContent(/x/i);
+    expect(xPlayerButton).toHaveClass(styles.playerX);
   });
 
   test("square has player's O css class color when mark is O", () => {
     const mockCallBack = jest.fn((squareIndex: number) => squareIndex);
     render(<Square mark="O" highlight={true} squareIndex={0} onSelectSquare={mockCallBack} />);
-    const xPlayer = screen.getByText(/O/);
+    const oPlayerButton = screen.getByRole("button");
 
-    expect(xPlayer).toHaveClass(styles.playerO);
+    expect(oPlayerButton).toHaveTextContent(/o/i);
+    expect(oPlayerButton).toHaveClass(styles.playerO);
   });
 });
